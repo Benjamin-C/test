2c34355a450803624e0ea03adefd57b7711193b6
Benjamin-C <bencrall@gmail.com>
2018-08-27 19:22:53 -0700

    init

---
+++ src/test/DeathChest.java
@@ -0,0 +1,18 @@
+package test;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DeathChest extends JavaPlugin {
+
+	// Fired when plugin is first enabled
+    @Override
+    public void onEnable() {
+
+    }
+    
+    // Fired when plugin is disabled
+    @Override
+    public void onDisable() {
+
+    }
+}
